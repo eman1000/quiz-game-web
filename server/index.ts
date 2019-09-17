f12b5d0163877f0ac96bda4d82ef5bad2fd389b7
Emmancipate Musemwa <emman@sam-media.com>
2019-09-17 08:43:59 +0800

    remove port

--- server/index.ts
+++ server/index.ts
@@ -44,7 +44,7 @@ const getMe = async req => {
 const db = createModels();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
