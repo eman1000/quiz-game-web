6b5002b28b4e7a09e0227617029b4c744b63bec1
emman <emman@sam-media.com>
2020-09-28 23:38:56 +0800

    build

---
+++ build/precache-manifest.46d2ad61f9add698857d99d915b32075.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8395f314c7d06032c0425482639562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97e4639162126eafde0",
+    "url": "/static/css/homepage.88b3c800.chunk.css"
+  },
+  {
+    "revision": "b8f77aab40a99c552f5e",
+    "url": "/static/css/main.5ba9cddf.chunk.css"
+  },
+  {
+    "revision": "73d13392e7bea8d793b0",
+    "url": "/static/js/4.b1320c43.chunk.js"
+  },
+  {
+    "revision": "9fd25ac356554c1eda86",
+    "url": "/static/js/5.1e924b79.chunk.js"
+  },
+  {
+    "revision": "e97e4639162126eafde0",
+    "url": "/static/js/homepage.47895e4d.chunk.js"
+  },
+  {
+    "revision": "acdd636f72b6cb6151ab",
+    "url": "/static/js/logout.c06c45c5.chunk.js"
+  },
+  {
+    "revision": "b8f77aab40a99c552f5e",
+    "url": "/static/js/main.effa3a0f.chunk.js"
+  },
+  {
+    "revision": "d08cb8c390452589bb1c",
+    "url": "/static/js/runtime~main.05152d2b.js"
+  },
+  {
+    "revision": "6dda1365bcd839dfa942aa8ca66c0a28",
+    "url": "/static/media/arrow.6dda1365.svg"
+  },
+  {
+    "revision": "f410d02dcfaa904512a41faa6aa982d7",
+    "url": "/static/media/box-bg.f410d02d.svg"
+  },
+  {
+    "revision": "f0ba1ec6b57ab220fd45786f765ac22c",
+    "url": "/static/media/brain.f0ba1ec6.svg"
+  },
+  {
+    "revision": "cdbd1ac476dd271e6b93342d72852db0",
+    "url": "/static/media/bronz.cdbd1ac4.png"
+  },
+  {
+    "revision": "325d35325bedb295c5e886330d507040",
+    "url": "/static/media/chest.325d3532.svg"
+  },
+  {
+    "revision": "578d3547588daf52a342fd35c83f26ed",
+    "url": "/static/media/coin.578d3547.svg"
+  },
+  {
+    "revision": "8ca770f8abe86abd8e09de81b2ca3356",
+    "url": "/static/media/complete.8ca770f8.svg"
+  },
+  {
+    "revision": "5fd82a7d5f8dce78363896fd48be260e",
+    "url": "/static/media/diamond.5fd82a7d.svg"
+  },
+  {
+    "revision": "5cd26013244f9510b79fa29aa42b52cd",
+    "url": "/static/media/diploma.5cd26013.svg"
+  },
+  {
+    "revision": "cc704e412c7f940414eb23e01ffd4501",
+    "url": "/static/media/filmstrip-icon.cc704e41.svg"
+  },
+  {
+    "revision": "59aeccfbb8ec9076992fa8972ae2d188",
+    "url": "/static/media/food-icon.59aeccfb.svg"
+  },
+  {
+    "revision": "6f4f76ba25e95206609b6dda3058c66c",
+    "url": "/static/media/germ.6f4f76ba.svg"
+  },
+  {
+    "revision": "8becb7053d1a9d047f4b748a677dbb73",
+    "url": "/static/media/gold.8becb705.png"
+  },
+  {
+    "revision": "e8b941fcae3d5161f721c90436068bf8",
+    "url": "/static/media/graduation.e8b941fc.svg"
+  },
+  {
+    "revision": "41495ed7c330e6657f1119f083503f52",
+    "url": "/static/media/ground.41495ed7.svg"
+  },
+  {
+    "revision": "189c6b70d32efc4c31cd7b11e95e0fd5",
+    "url": "/static/media/icon-back.189c6b70.svg"
+  },
+  {
+    "revision": "2a8cde6786ef9a10a4ad38cf3c479d43",
+    "url": "/static/media/logo.2a8cde67.svg"
+  },
+  {
+    "revision": "2d300590d1316e0f163f30feaf0999b3",
+    "url": "/static/media/medical-bag.2d300590.svg"
+  },
+  {
+    "revision": "f33b97d68fd9cbbf1ca3645f4c2d56c5",
+    "url": "/static/media/music-icon.f33b97d6.svg"
+  },
+  {
+    "revision": "b2922350f8a44d31a74a0eac4ceac635",
+    "url": "/static/media/orange-box.b2922350.svg"
+  },
+  {
+    "revision": "4938d4d759f1595eb2bdbb23bd1bee33",
+    "url": "/static/media/paw-icon.4938d4d7.svg"
+  },
+  {
+    "revision": "c2f348f98d17c9e1bcbe92623cb6aa51",
+    "url": "/static/media/silver.c2f348f9.png"
+  },
+  {
+    "revision": "a7547ec437b77b9f4839b446c8d32785",
+    "url": "/static/media/stars.a7547ec4.svg"
+  },
+  {
+    "revision": "4845841307a8b903363af361d73988dc",
+    "url": "/static/media/trophie.48458413.svg"
+  }
+]);
